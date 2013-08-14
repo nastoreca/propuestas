48eea9237fba3def56eee5cbcb042eb83547c572
Ignacio Astoreca Ronda <nastoreca@gmail.com>
2013-08-14 18:28:40 +0200

    Inicio de proyecto
    Primera subida de inicio de proyecto y estructura

---
+++ app/views/propuestas.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>What You're propousing</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="iastoreca" >
+
+    <!-- Le styles -->
+    <link href="http://twitter.github.io/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+
+      @media (min-width: 767px) {
+        .row-fluid {
+          padding-top: 20px;  
+        }
+      }
+
+      .color-fondo-body {
+        background-color: #EAEAEA;
+      }
+
+      .sombra {
+        -webkit-box-shadow:  3px 3px 13px #888;
+        box-shadow:  3px 3px 13px #888;
+      }
+
+      .color-fondo-mes {
+        background-color: #ffffff;
+      }
+    
+      .table-mes {
+        border-collapse: separate;  
+        border-spacing: 3px;
+      }
+
+      .table-mes td , .table-mes th {
+        border-top: 0px;
+        -webkit-border-radius: 0px;
+        border-radius: 0px;
+      }
+
+      .color-ene {
+        /*text-align: right;*/
+        background-color: #F3A516;
+      } 
+
+      .color-font-ene {
+        color: #F3A516;
+      }
+
+      .color-feb {
+        background-color: #47E147;
+      }
+
+      .color-font-feb {
+        color: #47E147;
+      }
+
+      .color-mar {
+        background-color: #ffffff;
+      }
+
+      .color-dia-sel {
+        background-color: #EAEAEA;
+        -webkit-box-shadow:  -1px -1px 2px #888;
+        box-shadow:  -1px -1px 2px #888;
+      }
+      
+      .fila-vacia td {
+        background-color: #ffffff;
+        -webkit-border-radius: 0px;
+        border-radius: 0px;
+        -webkit-box-shadow:  0px 0px 0px #888;
+        box-shadow:  0px 0px 0px #888;
+      }      
+      
+    </style>
+    
+    <link href="http://twitter.github.io/bootstrap/assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://twitter.github.io/bootstrap/assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://twitter.github.io/bootstrap/assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://twitter.github.io/bootstrap/assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://twitter.github.io/bootstrap/assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="http://twitter.github.io/bootstrap/assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="http://twitter.github.io/bootstrap/assets/ico/favicon.png">
+  </head>
+
+  <body class="color-fondo-body">
+
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Propouse</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+
+     <div class="row-fluid">   
+  
+      <div class="span4 sombra color-fondo-mes">
+      
+        <table class="table table-condensed table-mes">
+          <caption><h4 class="text-left color-font-ene">Enero</h4></caption>
+          <thead>
+            <tr>
+              <th class="color-font-ene">L</th>
+              <th class="color-font-ene">M</th>
+              <th class="color-font-ene">X</th>
+              <th class="color-font-ene">J</th>
+              <th class="color-font-ene">V</th>
+              <th class="color-font-ene">S</th>
+              <th class="color-font-ene">D</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="color-ene">1</td>
+              <td class="color-ene">2</td>
+              <td class="color-ene">3</td>
+              <td class="color-ene">4</td>
+              <td class="color-ene">5</td>
+              <td class="color-ene">6</td>  
+              <td class="color-ene">7</td>            
+            </tr>
+            <tr>
+              <td class="color-ene">8</td>
+              <td class="color-ene">9</td>
+              <td class="color-ene">10</td>                 
+              <td class="color-ene">11</td>
+              <td class="color-ene">12</td>
+              <td class="color-ene">13</td>
+              <td class="color-ene">14</td>                                                                       
+            </tr>
+            <tr>
+              <td class="color-ene">15</td>
+              <td class="color-ene">16</td>
+              <td class="color-ene">17</td>                 
+              <td class="color-ene">18</td>
+              <td class="color-ene">19</td>
+              <td class="color-ene">20</td>
+              <td class="color-ene">21</td>                                                                       
+            </tr>
+            <tr>
+              <td class="color-ene">22</td>
+              <td class="color-ene">23</td>
+              <td class="color-font-ene">24</td>                  
+              <td class="color-ene">25</td>
+              <td class="color-ene">26</td>
+              <td class="color-ene">27</td>
+              <td class="color-ene">28</td>                                                                       
+            </tr>
+            <tr>
+              <td class="color-ene">29</td>
+              <td class="color-ene">30</td>
+              <td class="color-ene">31</td>                 
+              <td class="color-ene"></td>
+              <td class="color-ene"></td>
+              <td class="color-ene"></td>
+              <td class="color-ene"></td>                                                                       
+            </tr>
+            <tr class="fila-vacia">
+              <td class="color-ene" colspan="7">&nbsp;</td>
+            </tr>
+            
+          </tbody>
+        </table>
+        
+      </div>
+      
+      <div class="span4 sombra color-fondo-mes">
+      
+        <table class="table table-condensed table-mes">
+          <caption><h4 class="text-left color-font-feb">Febrero</h4></caption>
+          <thead>
+            <tr>
+            <th class="color-font-feb">L</th>
+            <th class="color-font-feb">M</th>
+            <th class="color-font-feb">X</th>
+            <th class="color-font-feb">J</th>
+            <th class="color-font-feb">V</th>
+            <th class="color-font-feb">S</th>
+            <th class="color-font-feb">D</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="color-feb">1</td>
+              <td class="color-feb">2</td>
+              <td class="color-feb">3</td>
+              <td class="color-feb">4</td>
+              <td class="color-feb">5</td>
+              <td class="color-feb">6</td>  
+              <td class="color-feb">7</td>            
+            </tr>
+            <tr>
+              <td class="color-feb">8</td>
+              <td class="color-feb">9</td>
+              <td class="color-feb">10</td>                 
+              <td class="color-feb">11</td>
+              <td class="color-feb">12</td>
+              <td class="color-font-feb">13</td>
+              <td class="color-feb">14</td>                                                      
+            </tr>
+            <tr>
+              <td class="color-feb">15</td>
+              <td class="color-feb">16</td>
+              <td class="color-feb">17</td>                 
+              <td class="color-feb">18</td>
+              <td class="color-feb">19</td>
+              <td class="color-feb">20</td>
+              <td class="color-feb">21</td>                                                          
+            </tr>
+            <tr>
+              <td class="color-feb">22</td>
+              <td class="color-feb">23</td>
+              <td class="color-feb">24</td>                 
+              <td class="color-feb">25</td>
+              <td class="color-feb">26</td>
+              <td class="color-feb">27</td>
+              <td class="color-feb">28</td>                                                               
+            </tr>
+            <tr>
+              <td class="color-feb">29</td>
+              <td class="color-feb">30</td>
+              <td class="color-feb">31</td>                 
+              <td class="color-feb"></td>
+              <td class="color-feb"></td>
+              <td class="color-feb"></td>
+              <td class="color-feb"></td>                                                                       
+            </tr>
+            <tr class="fila-vacia">
+              <td class="color-feb" colspan="7">&nbsp;</td>
+            </tr>
+            
+          </tbody>
+        </table>
+        
+      </div>
+  
+        <div class="span4 sombra color-mar">
+          <h4>Marzo</h4>
+        </div>
+        
+     </div>
+     
+     <div class="row-fluid">
+     
+        <div class="span4 sombra color-mar">
+          <h4>Abril</h4>
+        </div>
+  
+     
+     </div>
+         
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://twitter.github.io/bootstrap/assets/js/jquery.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-transition.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-alert.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-modal.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-dropdown.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-tab.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-tooltip.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-popover.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-button.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-collapse.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-carousel.js"></script>
+    <script src="http://twitter.github.io/bootstrap/assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
